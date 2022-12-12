720f39601231970d25c3b852ce845f3d186385b5
Дмитрий Ткаченко <dmitrytkachenko7397@gmail.com>
2022-12-12 18:37:53 +0100

    Делает адаптив не главной странице

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,17 @@
       refs.modal.classList.toggle("is-hidden");
     }
   })();
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      bodyFixed: document.querySelector("[data-body-fixed]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.bodyFixed.classList.toggle("is-fixed");
+    }
+  })();
